0caf40b588ad2918497f65b4d60ee56f8665892d
kkingkkang0608-code <kkingkkang0608@gmail.com>
2026-02-03 15:07:45 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// 나중에 이미지를 추가하고 싶을 때 이 배열만 수정하면 됩니다!
+const works = [
+    { src: "images/001.png", title: "치비 일러스트1" },
+    { src: "images/002.png", title: "치비 일러스트2" }
+];
+
+const gallery = document.getElementById('gallery');
+
+works.forEach(work => {
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML = `
+        <img src="${work.src}" alt="${work.title}">
+    `;
+    gallery.appendChild(card);
+});
